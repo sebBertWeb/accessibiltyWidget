21a01cde97f7b1176666084bc1d7a39bf935293a
sebBertWeb <bert0380@algonquinlive.com>
2022-12-23 16:33:24 -0500

    single linedess

--- accessibility.js
+++ accessibility.js
@@ -32,25 +32,15 @@ function createWidget(pColor, sColor, hColor)   {
     position: sticky;
     bottom: 2vw;
     margin-right: 2vw;
-    display:-webkit-box;
-    display:-ms-flexbox;
-    display:flex;
+    display:-webkit-box; display:-ms-flexbox; display:flex;
     -webkit-box-orient: vertical;
-    -webkit-box-direction: reverse;
-    -ms-flex-direction: column-reverse;
-    flex-direction: column-reverse;
-    -webkit-box-align: end;
-    -ms-flex-align: end;
-    align-items: flex-end;
+    -webkit-box-direction: reverse; -ms-flex-direction: column-reverse; flex-direction: column-reverse;
+    -webkit-box-align: end; -ms-flex-align: end; align-items: flex-end;
     `;
     optionsContainer.style.cssText = `
-    display:-webkit-box;
-    display:-ms-flexbox;
-    display:flex;
+    display:-webkit-box; display:-ms-flexbox; display:flex;
     -webkit-box-orient: vertical;
-    -webkit-box-direction: reverse;
-    -ms-flex-direction: column-reverse;
-    flex-direction: column-reverse;
+    -webkit-box-direction: reverse; -ms-flex-direction: column-reverse; flex-direction: column-reverse;
     `;
     let btns = [mainBtn, btn1, btn2, btn3, btn4];
     btns.forEach((btn) => {
@@ -73,22 +63,66 @@ function createWidget(pColor, sColor, hColor)   {
     optionsContainer.appendChild(btn4);
     mainContainer.appendChild(mainBtn);
     mainContainer.appendChild(optionsContainer);
-    document.querySelector('#accessibilityWidget').appendChild(mainContainer);
+    document.body.appendChild(mainContainer);
     
 }
-createWidget('#121','#CDC');
+document.body.onload(createWidget('#121','#CDC'));
+/*
 // Events
+// Dyslexia storing, setting, and toggling
+if (localStorage.getItem('dys')==='y') { 
+    $('body').addClass('dyslexia');
+    $('.dyslexiaButton').addClass('currentPage');   // Adds Style
+}
+$('.dyslexiaButton').click(function() { 
+    accesibilityOnClick('dys','dyslexia','body');
+    $('.dyslexiaButton').toggleClass('currentPage');    // Since we add style if the user has dyslexia activated in storage on page open, we jsut need to toggle 
+});
+// Bigger Text storing, setting, and toggling
+if (localStorage.getItem('bigT')==='y') { 
+    $('body').addClass('biggerText');
+    $('.biggerTextButton').addClass('currentPage');
+}
+$('.biggerTextButton').click(function() { 
+    accesibilityOnClick('bigT','biggerText','body');
+    $('.biggerTextButton').toggleClass('currentPage');
+ });
+// Reading Helper
+if (!/Android|iPhone/i.test(navigator.userAgent)) { //check that user is not on mobile browser
+    if (localStorage.getItem('rg')==='y') { 
+        $('.readingGuide').addClass('rgDisplay');
+        $('.readingGuideButton').addClass('currentPage');
+    }
+    $('.readingGuideButton').click(function() { 
+        accesibilityOnClick('rg','rgDisplay','.readingGuide');
+        $('.readingGuideButton').toggleClass('currentPage');
+    });
+}
+if (localStorage.getItem('inv')==='y') { 
+    $('body').addClass('invertColor'); 
+}
+$('.inverseButton').click(function() {
+    accesibilityOnClick('inv','invertColor','body');
+});
+// Position for reading helper
+$('body').on('mousemove', function(e) { 
+    if (localStorage.getItem('rg')==='y') {                         // Check to save performance
+        $('.readingGuide').css({'top': e.pageY-20,'left': e.pageX-270});
+    }
+});
+// Hiding button if it is opened and use clicks away
+$('body').children().not('.accessibility').click(function() { 
+    $('.accessibility').addClass('accessibilityHidden'); 
+});
+// Toggling accesibility buttons showing/hiding
+$('.accessButton').click(function() { 
+    $('.accessibility').toggleClass('accessibilityHidden');
+});
 
-/*HTML
-<div id='accessibilityContainer' class='accessibilityHidden'>    maybe just do hide on options
-    <button id='accessibilityBtn'><img src='images\accessibility.svg' alt='Accessibility Options Button'></button>
-    <div id='accesibilityOptions'>
-        <button id='dysBtn'>Dyslexia Font</button>
-        <button id='bigBtn'>Bigger Text</button>
-        <button id='rgBtn'>Reading Guide</button>
-    </div>
-</div>
-<div id='readingGuide'>
-    <div id='readingGuideTriangleOuter'></div>
-    <div id='readingGuideTriangleInner'></div>
-</div>*/
+// Takes in a key, and className. Sets value to key, uses class to check and toggle (Also what element the class will be added to )
+function accesibilityOnClick(key,c,target)  {
+    $(target).toggleClass(c);
+    let val = 'n';  // Skips need for an else statement, more efficient
+    if ($(target).hasClass(c)) { val = 'y'; }
+    localStorage.setItem(key,val);
+}*/
